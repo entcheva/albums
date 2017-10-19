988d3442d0b2ea22fb823c53faf52b7e616c737e
entcheva <christina.entcheva@gmail.com>
2017-10-19 10:50:22 -0400

    Add component-level state for album data

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
 class AlbumList extends Component {
+  state = { albums: [] };
+
   componentWillMount() {
     fetch('https://rallycoding.herokuapp.com/api/music_albums')
     .then(response => response.json())
     .then((responseData) => {
-      console.log(responseData);
-    });
+      this.setState({ albums: responseData });
+    })
   }
 
   render() {
+    console.log(this.state);
+
     return (
       <View>
         <Text>hi</Text>
